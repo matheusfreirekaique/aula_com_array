b689f29b01bd3d318d2e1e6f0e71172874aa0f80
Matheus Freire <santosmatheus99@hotmail.com>
2023-05-29 20:31:05 -0300

    mod ficação do ex

--- test.py
+++ test.py
@@ -1,14 +1,11 @@
+import random
+
 lista = []
 
 n = int(input('insira a quantidade de nuemeros: '))
 
 for i in range(0,n):
-    lista.append(n)
-
-valor = 100 
-
-for i in range(len(listas)):
-    lista[i] *= valor
+    aleatoriro = int(random.random() * 100)
+    lista.append(aleatoriro)
     
 print(lista)
-
