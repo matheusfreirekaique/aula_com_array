46517ce59adfdfca642563ee0d23ec1823aea3df
Matheus Freire <santosmatheus99@hotmail.com>
2023-06-06 21:21:18 -0300

    alteração

--- ativi.py
+++ ativi.py
@@ -6,9 +6,16 @@ jogadores = []
 
 for i in range(quntidade_jogadores):
     nome = input(f'Digite o nome dos jogadores {i+1}:')
-    aleatorio = (random.randint(1,10))
+    aleatorios = (random.randint(1,10))
     jogadores.append(nome)
-    jogadores.append(aleatorio)
-    
-print('Os jogadores:', jogadores)
-print('Os valore aleatorio obitiodos por cada jogador é:', aleatorio)
+    jogadores.append(aleatorios)
+
+valor_vencedor = 0
+maior_numero = jogadores[0]
+
+for i in range(1, quntidade_jogadores):
+    if jogadores[i] > maior_numero:
+        maior_numero = jogadores[i]
+        valor_vencedor = i
+
+print('Os jogadores:', jogadores[valor_vencedor])
