d8c5e35a5983dd4f2bba55d70740116ab8e187de
Matheus Freire <santosmatheus99@hotmail.com>
2023-06-06 21:30:44 -0300

    atividade de duas variaves onde o maior valor deve ser guardado e imprimido

---
+++ exp.py
@@ -0,0 +1,17 @@
+# como faço para saber um numero é maior ou menor que outro?
+# como guardo um numero numa variavel 
+
+# a = 10
+# b = 20
+
+# como faço pra decidir quais dessas duas variaveis  tem o maior numero?
+
+a = 10
+b = 20
+
+if a > b:
+    maiorNumero = a
+else:
+    maiorNumero = b
+
+print(maiorNumero)
